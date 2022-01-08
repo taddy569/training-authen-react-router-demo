490079f39d65bfa675276a8b1526f39e59bf2de0
taddy569 <bendev5369@gmail.com>
2022-01-08 22:59:41 +0700

    remove ưebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { ReactQueryDevtools } from "react-query/devtools";
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { store } from "./redux/store";
 import { queryClient } from "./queries";
 
@@ -28,4 +27,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
